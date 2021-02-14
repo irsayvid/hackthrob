b353dee7457ca7a6cb8419561ecc34c211e0ef78
pkdiv <pkdivyesh@gmail.com>
2021-02-14 14:27:56 +0530

    cockroach db code added

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+var async = require('async');
+var fs = require('fs');
+var pg = require('pg');
 
 const app = express();
+
+
+var config = {
+  user: 'root',
+  host: 'localhost',
+  database: 'hackthrob',
+  port: 26257
+};
+
+
+var pool = new pg.Pool(config);
+
+pool.connect(function (err, client, done) {
+
+  // Close communication with the database and exit.
+  var finish = function () {
+      done();
+      process.exit();
+  };
+
+  if (err) {
+      console.error('could not connect to cockroachdb', err);
+      finish();
+  }
+  async.waterfall([
+      function (results, next) {
+              // Print out account balances.
+              client.query('SELECT * FROM users;', next);
+          },
+      ],
+      function (err, results) {
+          if (err) {
+              console.error('Error inserting into and selecting from accounts: ', err);
+              finish();
+          }
+
+          console.log('Initial balances:');
+          results.rows.forEach(function (row) {
+              console.log(row);
+          });
+
+          finish();
+      });
+});
 
 app.set("view engine","ejs")
 app.use(bodyParser.urlencoded({extended : true}));
