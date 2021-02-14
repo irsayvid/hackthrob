f79d7c6b19e13480463c7e42fad46f059ef68642
sairaj <sairaj2119@gmail.com>
2021-02-14 08:03:09 +0530

    add UI changfes

--- js/custom.js
+++ js/custom.js
@@ -1,117 +1,152 @@
-
-
 /* navigation menu animation with way points */
 
-$('.nav-animate').waypoint(function(direction) {
-	$('.secondary-menu').toggleClass('hide', direction === "down");
-	$('.navbar').toggleClass('comeup', direction === "down");
-}, {
-	offset: '10%'
-});
+$('.nav-animate').waypoint(
+  function (direction) {
+    $('.secondary-menu').toggleClass('hide', direction === 'down');
+    $('.navbar').toggleClass('comeup', direction === 'down');
+  },
+  {
+    offset: '10%',
+  }
+);
 
 // Block scrolling
-	
-$('.nav li a').bind('click', function(e){
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
-		}, 1200);
-		if($(window).width() < 768){
-			var $navMain = $(".navbar-collapse");
-			$navMain.collapse('hide');
-		}
-		e.preventDefault();
+
+$('.nav li a').bind('click', function (e) {
+  var anchor = $(this);
+  $('html, body')
+    .stop()
+    .animate(
+      {
+        scrollTop: $(anchor.attr('href')).offset().top,
+      },
+      1200
+    );
+  if ($(window).width() < 768) {
+    var $navMain = $('.navbar-collapse');
+    $navMain.collapse('hide');
+  }
+  e.preventDefault();
 });
 
 /* play list music button */
-$(document).ready(function(){
-	
-	var obj = document.createElement("audio");
-	obj.src = "../HTML/audio/audio.mp3";
-	obj.volume = 1;
-	obj.autoPlay = true;
-	obj.preLoad = true;       
-	
-	$('#playNowBtn').click(function(e){
-		var $playNowButton = $(this);																/* button variable */
-		var $playlist = $playNowButton.parent().parent();						/* play list section class */
-		var $disk			= $playlist.children().children('.disk');			/* disk image */
-		
-		if ($disk.hasClass('rotating')) {
-			$disk.removeClass('rotating');
-			$playNowButton.children('i').removeClass('fa-pause').addClass('fa-play');
-			obj.pause();
-		} else {
-			$disk.addClass('rotating');
-			$playNowButton.children('i').removeClass('fa-play').addClass('fa-pause');
-			obj.play();
-		}
-		e.preventDefault();
-	});
-	
+$(document).ready(function () {
+  var obj = document.createElement('audio');
+  obj.src = '../HTML/audio/audio.mp3';
+  obj.volume = 1;
+  obj.autoPlay = true;
+  obj.preLoad = true;
+
+  $('#playNowBtn').click(function (e) {
+    var $playNowButton = $(this); /* button variable */
+    var $playlist = $playNowButton
+      .parent()
+      .parent(); /* play list section class */
+    var $disk = $playlist.children().children('.disk'); /* disk image */
+
+    if ($disk.hasClass('rotating')) {
+      $disk.removeClass('rotating');
+      $playNowButton.children('i').removeClass('fa-pause').addClass('fa-play');
+      obj.pause();
+    } else {
+      $disk.addClass('rotating');
+      $playNowButton.children('i').removeClass('fa-play').addClass('fa-pause');
+      obj.play();
+    }
+    e.preventDefault();
+  });
 });
- 
+
 /* *************************************** */
-// One page navigation 
+// One page navigation
 /* *************************************** */
 
 $('.nav').onePageNav({
-    currentClass: 'active',
-    changeHash: true,
-    scrollSpeed: 1000,
-    scrollThreshold: 0.1
+  currentClass: 'active',
+  changeHash: true,
+  scrollSpeed: 1000,
+  scrollThreshold: 0.1,
 });
 
 // Block scrolling
-	
-$('.nav a').bind('click', function(e){
-		if($(window).width() < 768){
-			var $navMain = $(".navbar-collapse");
-			$navMain.collapse('hide');
-		}
-		e.preventDefault();
+
+$('.nav a').bind('click', function (e) {
+  if ($(window).width() < 768) {
+    var $navMain = $('.navbar-collapse');
+    $navMain.collapse('hide');
+  }
+  e.preventDefault();
 });
 
 /* Owl-Carousel Client Slider */
- 
-$(document).ready(function() {
-  $("#portfolioOwl").owlCarousel({
-		autoPlay: 3000,
-		slideSpeed: 1200,
-	  paginationSpeed : 500,
-	  stopOnHover: true,
-    items : 4,
-    itemsDesktop : [1199,4],
-    itemsDesktopSmall : [991,3],
-    itemsTabletSmall : [767,2]
-  }); 
+
+$(document).ready(function () {
+  $('#portfolioOwl').owlCarousel({
+    autoPlay: 3000,
+    slideSpeed: 1200,
+    paginationSpeed: 500,
+    stopOnHover: true,
+    items: 4,
+    itemsDesktop: [1199, 4],
+    itemsDesktopSmall: [991, 3],
+    itemsTabletSmall: [767, 2],
+  });
 });
 
 /* tool-tip initialize */
 
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
+/* Scroll to Top */
 
-/* Scroll to Top */  
+$('.totop').hide();
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $('.totop').fadeIn();
+    } else {
+      $('.totop').fadeOut();
+    }
+  });
 
-$(".totop").hide();
-$(function(){
-	$(window).scroll(function(){
-		if ($(this).scrollTop()>300)
-		{
-			$('.totop').fadeIn();
-		} 
-		else
-		{
-			$('.totop').fadeOut();
-		}
-	});
+  $('.totop a').click(function (e) {
+    e.preventDefault();
+    $('body,html').animate({ scrollTop: 0 }, 1200);
+  });
+});
 
-	$('.totop a').click(function (e) {
-		e.preventDefault();
-		$('body,html').animate({scrollTop: 0}, 1200);
-	});
+function makeid(length) {
+  var result = '';
+  var characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
+function genId() {
+  const unqid = document.getElementById('unique-id');
+  unqid.innerHTML = makeid(30);
+}
+
+const genIdBtn = document.querySelector('#gen-id-btn');
+genIdBtn.addEventListener('click', () => {
+  genId();
+});
+
+function copyToClipboard(elementId) {
+  var aux = document.createElement('input');
+  aux.setAttribute('value', document.getElementById(elementId).innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand('copy');
+  document.body.removeChild(aux);
+}
+const copyIcon = document.getElementById('copy-icon');
+copyIcon.addEventListener('click', (e) => {
+  copyToClipboard('unique-id');
 });
